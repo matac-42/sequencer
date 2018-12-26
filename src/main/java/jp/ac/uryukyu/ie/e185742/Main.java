9d0024b8db983ea5f12a20c478169bbdaa5086be
Yuto Matayoshi <e185742@ie.u-ryukyu.ac.jp>
2018-12-26 19:57:55 +0900

    setting the gradle & create the method "SoundOutput"

--- src/main/java/jp/ac/uryukyu/ie/e185742/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e185742/Main.java
@@ -3,5 +3,9 @@ package jp.ac.uryukyu.ie.e185742;
 public class Main {
     public static void main(String[] args){
 
+        String bassDrumSet = "bassdrum.wav";//SoundSet内の音声ファイル名を入れる。
+        Sequencer sequencer = new Sequencer();
+        sequencer.SoundOutput(bassDrumSet);
+
     }
 }
--- src/main/java/jp/ac/uryukyu/ie/e185742/Sequencer.java
+++ src/main/java/jp/ac/uryukyu/ie/e185742/Sequencer.java
@@ -1,4 +1,22 @@
 package jp.ac.uryukyu.ie.e185742;
 
+import java.io.File;
+
+import javafx.scene.media.AudioClip;
+
 public class Sequencer {
+
+    /**
+     * Play the sound road from the outside.
+     * @param setName sound file name. ex) SoundSet/bassdrum.wav
+     */
+    public void SoundOutput(String setName){
+        AudioClip bassDrum = new AudioClip(new File("SoundSet/"+setName).toURI().toString());
+        bassDrum.play();
+        try{
+            Thread.sleep(30000);
+        }catch (InterruptedException e){
+        }
+    }
+
 }
